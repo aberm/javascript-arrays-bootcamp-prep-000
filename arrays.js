13d299ba28309d81373096d5a300d25bce85746f
Menachem Aber <abermendel@gmail.com>
2018-11-08 16:32:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,7 +17,7 @@ function addElementToEndOfArray(array, element) {
   return array2
 }
 
-function addElementToEndOfArray(array, element) {
+function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
 }
