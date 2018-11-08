8848acf3e4722999871c9c3191eda5d415228c41
Menachem Aber <abermendel@gmail.com>
2018-11-08 16:26:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,6 @@ var chocolateBars = [
   'snickers', 'hundred grand', 'kitkat', 'skittles'
   ]
   
-  function addElementToBeginningOfArray(a, e) {
-    
-  }
+function addElementToBeginningOfArray(a, e) {
+  
+}
